a4cd47e6ce8d43922c94b50c1d99f0384bdbc09c
Arsen <arsenminer@gmail.com>
2023-02-17 16:58:13 +0600

    test

--- controllers/userController.go
+++ controllers/userController.go
@@ -3,10 +3,14 @@ package controllers
 import (
 	"REST_API/database"
 	"REST_API/helpers"
+	"REST_API/models"
+	"context"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"net/http"
+	"time"
 )
 
 var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
@@ -20,8 +24,25 @@ func VerifyPassword() {
 
 }
 
-func SignUp() {
+func SignUp() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
 
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		validationErr := validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+
+		count, err := userCollection.CountDocuments(ctx, bson.M())
+
+	}
 }
 
 func Login() {
@@ -40,6 +61,17 @@ func GetUser() gin.HandlerFunc {
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), time.Second*100)
+
+		var user models.User
+		userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+
+		c.JSON(http.StatusOK, user)
 
 	}
 }
--- helpers/authHelper.go
+++ helpers/authHelper.go
@@ -5,6 +5,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func CheckUserType(c *gin.Context, role string) (err error) {
+	userType := c.GetString("user_type")
+	err = nil
+
+	if userType != role {
+		err = errors.New("Unauthorized to aces this resource")
+		return err
+	}
+
+	return err
+}
+
 func MatchUserTypeToUid(c *gin.Context, userId string) (err error) {
 	userType := c.GetString("user_type")
 	uid := c.GetString("uid")
@@ -15,5 +27,8 @@ func MatchUserTypeToUid(c *gin.Context, userId string) (err error) {
 		err = errors.New("Unauthorized to aces this resource")
 		return err
 	}
+
+	err = CheckUserType(c, userType)
+	return err
 
 }
